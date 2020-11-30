5dded7352f2b36b191381ec2f747f208dcac3926
ErhnKrl <ye.koral@gmail.com>
2020-11-30 05:45:05 +0300

    Basic React

---
+++ First Component/index.js
@@ -0,0 +1,11 @@
+
+
+class Hello extends React.Component {
+    render () {
+        return (
+            <h1>Hello!</h1>
+        )
+    }
+}
+
+ReactDom.render(<Hello/>, document.getElementById('root'));
