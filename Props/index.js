2c06bbf439d40effc55598317160cbf423f13d78
ErhnKrl <ye.koral@gmail.com>
2020-12-01 03:56:48 +0300

    props

---
+++ Props/index.js
@@ -0,0 +1,14 @@
+
+class App extends React.Component {
+  render() {
+
+    return (
+      <div>
+        <Hello to="Jane" from="Jack" bangs={10}/>
+        <Hello to="Sarah" from="Paul" bangs={4} img="https://media.giphy.com/media/xUySTQZfdpSkIIg88M/giphy.gif"/>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
