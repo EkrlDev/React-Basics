1977d076b42d35f668c96ec157f62087c2ac4065
ErhnKrl <ye.koral@gmail.com>
2021-10-20 13:38:33 +0300

    yahtzee game fixing

--- yahtzee-starter/src/Die.js
+++ yahtzee-starter/src/Die.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+
+  static defaultProps = {
+    numberWords: ["one", "two", "three", "four", "five", "six"],
+    val:6
+  }
+
+  handleClick = () => {
+    this.props.handleClick(this.props.idx)
+  }
+    
   render() {
+    let classes = `Die fas fa-dice-${this.props.numberWords[this.props.val - 1]} fa-5x `;
+    if(this.props.locked) classes += "Die-locked ";
+    if(this.props.rolling) classes += "Die-rolling";
+
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.props.handleClick}
+      <i
+        className={classes}
+        onClick={this.handleClick}
       >
-        {this.props.val}
-      </button>
+      </i>
     );
   }
 }
